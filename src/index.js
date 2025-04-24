0a074e700b4bf2d97f3770dc64a34c0de8260bd0
Filipe Reges de Salles <filipesallesdev@gmail.com>
2025-04-24 09:46:08 -0300

    alterando o nome dos endpoints acrescentando api/v1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(userRoutes);
+app.use('api/v1',userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
