7895b4b40593bb90f45711d211b90749fa59e38b
Filipe Reges de Salles <filipesallesdev@gmail.com>
2025-04-24 15:53:32 -0300

    adicionando cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import instituicaoRoutes from './routes/instituicaoRoutes.js';
+import cors from 'cors';        
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', instituicaoRoutes);
