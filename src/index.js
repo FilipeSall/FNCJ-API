1431d27a9bb9052893eca5a5e458a2632d00da2a
Filipe Reges de Salles <filipesallesdev@gmail.com>
2025-04-24 10:16:15 -0300

    ajustando o PORT

--- src/index.js
+++ src/index.js
@@ -8,4 +8,7 @@ app.use(express.json());
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', instituicaoRoutes);
 
-app.listen(3000, () => console.log('API rodando em http://localhost:3000'));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`API ouvindo na porta ${PORT}`);
+});
